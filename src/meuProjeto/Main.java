8469fb753409328eddf592e9b905a1c13c8af956
WallaceBarrosDev <w.wallacebarro.cruz@gmail.com>
2025-03-25 21:46:25 +0000

    valores primitivos

--- src/meuProjeto/Main.java
+++ src/meuProjeto/Main.java
@@ -2,6 +2,18 @@ package meuProjeto;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("main test");
+        byte valorEmByte = 101;
+        short intPequeno = 10;
+        long intGrande = 1000;
+        
+        int inteiro = 100;
+
+        float fracionario = 2.33f;
+        double fracionarioX2 = 4.89783d;
+        
+        boolean vslorLogico = true;
+        char caractere = 'a';
+
+        System.out.println(caractere);
     }
 }
