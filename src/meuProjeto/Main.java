b36abb87cb07da7c18c72854374bca08c3c43204
WallaceBarrosDev <w.wallacebarro.cruz@gmail.com>
2025-03-25 21:46:46 +0000

    cast de valores

--- src/meuProjeto/Main.java
+++ src/meuProjeto/Main.java
@@ -2,18 +2,9 @@ package meuProjeto;
 
 public class Main {
     public static void main(String[] args){
-        byte valorEmByte = 101;
-        short intPequeno = 10;
-        long intGrande = 1000;
-        
-        int inteiro = 100;
+        float valorFracionario = 4.333f;
+        double valorFracionarioX2 = (double)valorFracionario;
 
-        float fracionario = 2.33f;
-        double fracionarioX2 = 4.89783d;
-        
-        boolean vslorLogico = true;
-        char caractere = 'a';
-
-        System.out.println(caractere);
+        System.out.println(valorFracionarioX2);
     }
 }
