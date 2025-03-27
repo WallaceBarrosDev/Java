fa76183355902bd705d2e4b5bc64a544b0ecff05
WallaceBarrosDev <w.wallacebarro.cruz@gmail.com>
2025-03-27 20:36:52 +0000

    feat: operador ternario

--- src/meuProjeto/Main.java
+++ src/meuProjeto/Main.java
@@ -2,17 +2,8 @@ package meuProjeto;
 
 public class Main {
     public static void main(String[] args){
-        // estrutura condicional
-
-        int a, b = 0;
-
-        a = 2;
-        b = 5;
-
-        if (a > b) {
-            System.out.println(a+" e maior que"+b);
-        } else {
-            System.out.println(a+" menor que b "+b);
-        }
+        int a = 10;
+        int b = 11;
+        System.out.println("resultado = "+(a > b ? a : b));
     }
 }
