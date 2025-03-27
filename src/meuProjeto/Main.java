1b95f9523a2ae521e88f08c288c3fceade6db0c7
WallaceBarrosDev <w.wallacebarro.cruz@gmail.com>
2025-03-27 20:52:17 +0000

    feat: switch

--- src/meuProjeto/Main.java
+++ src/meuProjeto/Main.java
@@ -2,8 +2,37 @@ package meuProjeto;
 
 public class Main {
     public static void main(String[] args){
-        int a = 10;
-        int b = 11;
-        System.out.println("resultado = "+(a > b ? a : b));
+        char dia = 'Q';
+        switch (dia) {
+            case 'D':
+                System.out.println("Domingo");
+            break;
+
+            case 's':
+                System.out.println("Segunda-feira");
+            break;
+
+            case 't':
+                System.out.println("Terça-feira");
+            break;
+
+            case 'q':
+                System.out.println("Quarta-feira");
+            break;
+
+            case 'Q':
+                System.out.println("Quinta-feira");
+            break;
+
+            case 'S':
+                System.out.println("Sexta-feira");
+            break;
+
+            case 'A':
+                System.out.println("Sabado");
+            break;
+            default:
+                System.out.println("valor invalido");
+        }
     }
 }
