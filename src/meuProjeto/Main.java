62c8618eaf9a6a8f60e142d03fda78be74149644
WallaceBarrosDev <w.wallacebarro.cruz@gmail.com>
2025-03-27 10:25:23 +0000

    feat: operaodres and, or e not

--- src/meuProjeto/Main.java
+++ src/meuProjeto/Main.java
@@ -2,16 +2,23 @@ package meuProjeto;
 
 public class Main {
     public static void main(String[] args){
-        //operadpres logicos
-        boolean maior = 2 > 1;
-        boolean menor = 1 < 2;
+        // operador or, and, not
+        
+        int a, b = 0;
+        boolean condicao = false;
+        a = 1; b = 2;
 
-        boolean mairoIgual = 2 >= 1;
-        boolean menorIgual = 1 >= 2;
+        // and 
+        condicao = a < b && b < a;
+        System.out.println(condicao);
 
-        boolean igual = 1 == 1;
-        boolean diferente = 1 != 2;
+        // or
+        condicao = a < b || a == b;
+        System.out.println(condicao);
 
-        System.out.println(diferente);
+        // not
+        condicao = a == 1;
+        System.out.println(condicao);
+        System.out.println(!condicao);
     }
 }
