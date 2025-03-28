4522b164fd2f8bcb47ae9e963b14c4900c8146a9
WallaceBarrosDev <w.wallacebarro.cruz@gmail.com>
2025-03-28 19:42:59 +0000

    matrizes

--- src/meuProjeto/Main.java
+++ src/meuProjeto/Main.java
@@ -1,14 +1,13 @@
 package meuProjeto;
 
 public class Main {
-    public static void main(String[] args){
-        int lethg = 10;
-        int[] numeros = new int[lethg];
-
-        for(int i = 0; i < lethg; i++){
-            numeros[i] = i+1;
-
-            System.out.println(numeros[i]);
+    public static void main(String[] args){ 
+        int[][] matriz = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(matriz[i][j]);
+            }
+            System.out.println("");
         }
     }
 }
