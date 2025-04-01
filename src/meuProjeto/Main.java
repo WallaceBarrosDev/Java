aee8532e83ca11bc9cb0e6b2b5d2768a6f03a29e
WallaceBarrosDev <w.wallacebarro.cruz@gmail.com>
2025-04-01 08:54:23 +0000

    feat: POO class

--- src/meuProjeto/Main.java
+++ src/meuProjeto/Main.java
@@ -1,13 +1,9 @@
-package meuProjeto;
+package src.meuProjeto;
+import src.myClasses.Test;
 
 public class Main {
-    public static void main(String[] args){ 
-        int[][] matriz = new int[3][3];
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(matriz[i][j]);
-            }
-            System.out.println("");
-        }
+    public static void main(String[] args){
+        Test test = new Test("Wallace");
+        test.hello();
     }
 }
---
+++ src/myClasses/Test.java
@@ -0,0 +1,13 @@
+package src.myClasses;
+
+public class Test {
+    private String name = " ";
+
+    public Test(String name){
+        this.name = name;
+    }
+
+    public void hello(){
+        System.out.println("Hello "+this.name);
+    }
+}
