f17b02312a6853225652093e03e69a7d512a1cc9
WallaceBarrosDev <w.wallacebarro.cruz@gmail.com>
2025-04-02 15:34:14 -0300

    a

--- src/myProject/Main.java
+++ src/myProject/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Employee employee = new Employee("Wallace", 22, 2300.0, 2640.20, 2400.30);
         employee.myInformation();
+        //a
     }
 }
