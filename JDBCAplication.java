19dec232630b9c702a59a10ae71fb5ffe393a92d
WallaceBarrosDev <w.wallacebarro.cruz@gmail.com>
2025-04-26 15:17:09 -0300

    fix: erro na coneção com o banco de dados

--- JDBCAplication.java
+++ JDBCAplication.java
@@ -4,14 +4,14 @@ import java.sql.SQLException;
 import java.sql.PreparedStatement;
 import java.util.ArrayList;
 
-public class DataBase {
-    private static final String table = "users";
+class DataBase {
+    private static final String database = "jdbcdb";
     private static final String port = "3306";
     private static final String endpont = "localhost";
     private static final String driver = "jdbc:mysql";
-    private static final String URL = driver + "://" + endpont + ":" + port + "/" + table;
+    private static final String URL = driver + "://" + endpont + ":" + port + "/" + database;
     private static final String user = "root";
-    private static final String password = "root";
+    private static final String password = "150203@Dc";
 
     private static Connection Connection = null;
     
@@ -30,31 +30,37 @@ public class DataBase {
     }
 }
 
-public class User {
-    private long id;
+class User {
+    private int id;
     private String name;
     private String email;
     private String password;
 
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
     public ArrayList<String> getInformation() {
-        ArrayList<String> userInformation = new ArrayList<String>();
-        userInformation.add(name);
-        userInformation.add(email);
-        userInformation.add(password);
-        return userInformation;
+        ArrayList<String> informations = new ArrayList<>();
+        informations.add(name);
+        informations.add(password);
+        informations.add(email);
+        return informations;
     }
 }
 
-public class UserService {
+class UserService {
     public static void sendUser(User user) {
-        final String sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+        final String sql = "INSERT INTO users (name, password, email) VALUES (?, ?, ?)";
         final ArrayList<String> userInfomation = user.getInformation();
 
         try {
             PreparedStatement preparedStatement = DataBase.getConnection().prepareStatement(sql);
-            preparedStatement.setString(1, userInfomation.name);
-            preparedStatement.setString(2, userInfomation.email);
-            preparedStatement.setString(3, userInfomation.password);
+            preparedStatement.setString(1, userInfomation.get(0));
+            preparedStatement.setString(2, userInfomation.get(1));
+            preparedStatement.setString(3, userInfomation.get(2));
             preparedStatement.execute();
             preparedStatement.close();
 
@@ -64,14 +70,9 @@ public class UserService {
     }
 }
 
-
-
 public class JDBCAplication {
     public static void main(String[] args) {
-        User user = new User();
-        user.setName("John Doe");
-        user.setEmail("jonhdoe@gmail.com");
-        user.setPassword("123456");
+        User user = new User("meu nome2", "meuEmail@gmail.com", "minha senha");
 
         UserService.sendUser(user);
         System.out.println("User created successfully");
